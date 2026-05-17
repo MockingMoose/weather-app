33d8eea349db49db49cabfd250660af1202cd26c
MockingMoose <198123013+MockingMoose@users.noreply.github.com>
2026-05-16 22:20:11 -0400

    adds survivalStatus bar with messages

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const apiKey = "a5d1109ea924711082ba56ce316686f3"
 let submitBtn = document.getElementById("submitBtn")
 let cityBox = document.getElementById("city")
 let weatherInfo = document.getElementById("weatherInfo")
+let status = document.getElementById("survivalStatus")
 
-submitBtn.addEventListener("click", getWeatherData);
+submitBtn.addEventListener("click", GetWeatherData);
 
-async function getWeatherData() {
-    let zip = document.getElementById("inputZIP").value
+async function GetWeatherData() {
+    let zip = document.getElementById("inputZIP").value;
     let geoRes = await fetch(
         `https://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${apiKey}`
     );
@@ -24,5 +25,22 @@ async function getWeatherData() {
     weatherInfo.innerHTML += " and ";
     weatherInfo.innerHTML += weatherData.weather[0].description;
 
-    
+    let temperature = weatherData.main.temp;
+    let description = weatherData.weather[0].description;
+    let message;
+
+    if (temperature <= 32){
+        message = "Freeze Imminent: Hypothermia risk high. Seek warmth immediately.";
+    } else if (temperature > 32 && temperature < 59){
+        message = "Cold Front: Thermal energy draining at increased rate. Equip thermal clothing.";
+    } else if (temperature >= 60 && temperature < 84){
+        message = "Optimal Conditions: Perfect time to forage.";
+    } else if (temperature >= 85 && temperature < 95){
+        message = "Uncomfortable Heat: Increased fluid consumption recommended.";
+    } else if (temperature >= 95){
+        message = "Scorched Earth: Hydration levels dropping at alarming rate. Seek shade immediately.";
+    };
+
+    console.log(status);
+    status.innerHTML = message;
 };
