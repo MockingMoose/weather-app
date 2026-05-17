62e0862a28f530d6038820701d07410ae95da2e9
MockingMoose <198123013+MockingMoose@users.noreply.github.com>
2026-05-16 22:31:35 -0400

    adds messages for wind speed

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ submitBtn.addEventListener("click", GetWeatherData);
 
 async function GetWeatherData() {
     let zip = document.getElementById("inputZIP").value;
+    //takes zip code to find lat and lon
     let geoRes = await fetch(
         `https://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${apiKey}`
     );
     let geoData = await geoRes.json();
     let { lat, lon } = geoData;
-    
+    //takes lat and lon to find weather data.
     let weatherRes = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
     );
@@ -24,11 +25,13 @@ async function GetWeatherData() {
     weatherInfo.innerHTML = weatherData.main.temp;
     weatherInfo.innerHTML += " and ";
     weatherInfo.innerHTML += weatherData.weather[0].description;
-
+    
     let temperature = weatherData.main.temp;
     let description = weatherData.weather[0].description;
+    let windSpeed = weatherData.wind.speed;
     let message;
 
+    //Checks temperature to choose correct message.
     if (temperature <= 32){
         message = "Freeze Imminent: Hypothermia risk high. Seek warmth immediately.";
     } else if (temperature > 32 && temperature < 59){
@@ -40,7 +43,14 @@ async function GetWeatherData() {
     } else if (temperature >= 95){
         message = "Scorched Earth: Hydration levels dropping at alarming rate. Seek shade immediately.";
     };
-
-    console.log(status);
+    
+    //Checks wind speed to choose correct message.
+    if (windSpeed < 10) {
+        message += "<br> Calm Breeze: No interference.";
+    } else if (windSpeed > 10 && windSpeed < 25) {
+        message += "<br> Gale Warning: Secure loose gear.";
+    } else if (windSpeed >= 25) {
+        message += "<br> Extreme Wind: Stay indoors.";
+    };
     status.innerHTML = message;
 };
